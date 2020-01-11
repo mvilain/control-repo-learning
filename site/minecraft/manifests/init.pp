47a186d9e8cbd54b4160720be8fdf3355e409696
Michael Vilain <mvilain@users.noreply.github.com>
2020-01-10 23:14:09 -0800

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -6,12 +6,12 @@ class minecraft {
   }
   
   notify { '/opt/minecraft/minecraft_server.jar':
-    message => 'downloading jar file...this may take a minute',
+    message => 'downloading...this may take a minute',
   } ->
   file { '/opt/minecraft/minecraft_server.jar':
     ensure  => file,
     source  => 'https://launcher.mojang.com/v1/objects/4d1826eebac84847c71a77f9349cc22afd0cf0a1/server.jar',
-    require  => File ['/opt/minecraft'],
+    require => File['/opt/minecraft'],
   }
   
   package { 'java':
@@ -21,19 +21,19 @@ class minecraft {
   file { '/opt/minecraft/eula.txt':
     ensure  => file,
     content => 'eula=true',
-    require  => File ['/opt/minecraft'],
+    require => File['/opt/minecraft'],
   }
   
   file { '/etc/systemd/system/minecraft.service':
     ensure  => file,
     source  => 'puppet:///modules/minecraft/minecraft.service',
-    require => Package [ 'java' ],
+    require => Package['java'],
   }
   
   service { 'minecraft':
     ensure  => running,
     enable  => true,
-    require => [ Package ['java'], File ['/etc/systemd/system/minecraft.service' ] ],
+    require => [ Package['java'], File['/etc/systemd/system/minecraft.service'] ],
   }
 
 }
