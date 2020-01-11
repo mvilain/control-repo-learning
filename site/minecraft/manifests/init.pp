ac7cbb7ab6010feb7f6b04d8598e0c295de4ac28
Michael Vilain <mvilain@users.noreply.github.com>
2020-01-10 23:10:22 -0800

    fix ordering

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -11,6 +11,7 @@ class minecraft {
   file { '/opt/minecraft/minecraft_server.jar':
     ensure  => file,
     source  => 'https://launcher.mojang.com/v1/objects/4d1826eebac84847c71a77f9349cc22afd0cf0a1/server.jar',
+    require  => File ['/opt/minecraft'],
   }
   
   package { 'java':
@@ -20,16 +21,19 @@ class minecraft {
   file { '/opt/minecraft/eula.txt':
     ensure  => file,
     content => 'eula=true',
+    require  => File ['/opt/minecraft'],
   }
   
   file { '/etc/systemd/system/minecraft.service':
-    ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
+    ensure  => file,
+    source  => 'puppet:///modules/minecraft/minecraft.service',
+    require => Package [ 'java' ],
   }
   
   service { 'minecraft':
-    ensure => running,
-    enable => true,
+    ensure  => running,
+    enable  => true,
+    require => [ Package ['java'], File ['/etc/systemd/system/minecraft.service' ] ],
   }
 
 }
