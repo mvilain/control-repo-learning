71356de419f618189cef682e972650ae6e93f706
Michael Vilain <mvilain@users.noreply.github.com>
2020-01-11 00:11:12 +0000

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -1,17 +1,17 @@
 class profile::ssh_server {
-	package {'openssh-server':
-		ensure => present,
-	}
-	service { 'sshd':
-		ensure => 'running',
-		enable => 'true',
+  package {'openssh-server':
+    ensure => present,
+  }
+  service { 'sshd':
+    ensure  => 'running',
+    enable  => 'true',
     require => Package[ 'openssh-server' ],
-	}
-	ssh_authorized_key { 'root@master.puppet.vm':
-		ensure => present,
-		user   => 'root',
-		type   => 'ssh-rsa',
-		key    => '',
+  }
+  ssh_authorized_key { 'root@master.puppet.vm':
+    ensure => present,
+    user   => 'root',
+    type   => 'ssh-rsa',
+    key    => '',
     notify => Service[ 'sshd' ],
-	}  
+  }  
 }
