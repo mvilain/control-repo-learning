67420f5dce759c0d94680f47ef624d1c3c2f63d5
Michael Vilain <mvilain@users.noreply.github.com>
2020-01-11 00:26:56 +0000

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -11,7 +11,7 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
-    notify => Service[ 'sshd' ],
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDS5+ZMgWCDG8CqrdlWHHPzVGrokgnCZdtvbR8KLyUUN+rdCt/N0bBG9lQ8K4IcjrcoJEImpYp7UEqtpgO+JVSNiZFZpc7V8A29E+2wBrCDnLF2vsaS90II4aqJJKMl+6wxZ+OanaKLm6rVN8BOYBf47C3nZXAxgwPvHWinayMNySbADKodKtKQNbOwFS7IILhHM03vwFJTTInKm37p/W9Gwn9YWJyXnjfek1J6fD2HHt1dqZ0G7IKs/LIHzbDie9fatQzW020oYjNIKI8SEhobZsg7tmIF6U/bkEL9nsMeHCvnPk2VLpZ3SMIdifeCaFoBeC1OuiYPqBuZE1gYEGY3',
+#    subscribe => Service[ 'sshd' ],
   }  
 }
