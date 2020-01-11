1e541aabf11540edf0970405f8e1920918412369
Michael Vilain <mvilain@users.noreply.github.com>
2020-01-11 00:49:21 +0000

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,14 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node { 'web.puppet.vm': }
   dockeragent::node { 'db.puppet.vm': }
+  host { 'web.puppet.vm':
+    ip           => '172.18.0.2',
+    ensure       => present,
+    host_aliases => 'web',
+  }
+  host { 'db.puppet.vm':
+    ip           => '172.18.0.3',
+    ensure       => present,
+    host_aliases => 'db',
+  }
 }
