476674b3c17a5b8b4111106fdc762d007feeb913
Michael Vilain <mvilain@users.noreply.github.com>
2020-01-10 21:49:54 -0800

    create mindcraft server

---
+++ site/mindcraft/manifests/init.pp
@@ -0,0 +1,27 @@
+class minecraft {
+  file{ '/opt/mindcraft':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+  }
+  
+  file { '/opt/mindcraft/mindcraft_server.jar':
+    ensure  => file,
+    source  => 'https://launcher.mojang.com/v1/objects/4d1826eebac84847c71a77f9349cc22afd0cf0a1/server.jar',
+  }
+  
+  package { 'java':
+    ensure  => present,
+  }
+  
+  file { '/opt/mindcraft/eula.txt':
+    ensure => file,
+    content => 'eula=true',
+  }
+  
+  file { 'etc/systemd/system/mindcraft.service':
+    ensure => file,
+    source => 'puppet:///modules/mindcraft/mindcraft.service',
+  }
+  # java -Xmx1024M -Xms1024M -jar minecraft_server.1.15.1.jar nogui 
+}
